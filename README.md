5d27ef43f7e4b14034cbdb9ca8bd7448be393e9a
infeeeee <gyetpet@mailbox.org>
2022-05-09 23:59:16 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,199 @@
 # ha-argos
-Put Home-asstisant to your top bar!
+
+Put Home-asstisant to your top bar! An [Argos](https://github.com/p-e-w/argos)/[Kargos](https://github.com/lipido/kargos)/Bitbar/[Xbar](https://github.com/matryer/xbar) script.
+
+Mac (Bitbar/Xbar) compatibility is not tested, but it should work. Open an issue if you are trying to use it, I would be happy to help.
+
+## Screenshots
+
+![](images/Screenshot%20from%202022-05-09%2023-54-36.png)
+
+## Comparison with other top bar clients
+
+None of the existing top bar clients were suitable for me, so I wrote this one.
+
+| Name                                                                                              | Platforms               | Features | Configuration |
+| ------------------------------------------------------------------------------------------------- | ----------------------- | -------- | ------------- |
+| ha-argos                                                                                          | Linux (Gnome, Kde), Mac | Lot      | yaml file     |
+| [ha-menu](https://github.com/addyire/ha-menu)                                                     | Mac, Windows            | Lot      | json file     |
+| [Home Assistant Extension](https://extensions.gnome.org/extension/4170/home-assistant-extension/) | Linux (Gnome)           | Limited  | Gui           |
+
+## Install
+
+1. Install argos, kargos, bitbar or xbar
+2. Clone the repo or download as zip
+3. Install python and dependencies
+4. Make sure `ha-argos.py` is executable
+5. Symlink `ha-argos.py` to the config dir
+6. Set up configuration.yaml
+
+### Example on a Debian based linux with pip:
+
+```shell
+sudo apt install python3 python3-pip
+git clone https://github.com/infeeeee/ha-argos
+cd ha-argos
+pip install -r requirements.txt
+chmod +x ha-argos.py
+ln -s `readlink -f .`/ha-argos.py ~/.config/argos/
+cp configuration.yaml.example configuration.yaml
+```
+
+### Example on Arch:
+
+```shell
+pacman -S python python-yaml python-requests python-lxml
+git clone https://github.com/infeeeee/ha-argos
+cd ha-argos
+chmod +x ha-argos.py
+ln -s `readlink -f .`/ha-argos.py ~/.config/argos/
+cp configuration.yaml.example configuration.yaml
+```
+
+## Configuration
+
+Everything is set up in the `configuration.yaml`. Just place it next to the script. you can start from `configuration.yaml.example`, just copy and edit it. 
+
+The variable names are based on Home-assistant's Lovelace yaml config.
+
+To reload
+
+### Basic configuration
+
+```yaml
+settings:
+  url: https://myserver.url
+  token: my-secret-token
+lines:
+  - entity: sensor.livingroom_temperature
+    attribute:
+      - state
+      - unit_of_measurement
+    icon: mdi:home-assistant
+  - separator
+  - entity: light.livingroom
+    service: light.toggle
+    data: 
+      brightness: 255
+  - name: Garage
+    icon: mdi:garage
+    entities:
+    - entity: cover.garage
+      service: cover.open_cover
+      name: Open garage door
+    - entity: cover.garage
+      service: cover.close_cover
+      name: Close garage door
+```
+
+## All config options
+
+### Settings
+
+```yaml
+settings:
+  url: https://myserver.url
+  token: my-secret-token
+  icon_color: ffffff
+  icon_size: 22
+```
+
+`url`: [required] The full url of your home-assistace instance. Include http(s) at the beginning and port umber at the end.
+
+`token`: [required] Your long-lived access token. You can generate this on your profile. [Official help.](https://www.home-assistant.io/docs/authentication/#your-account-profile)
+
+You can navigate to your ha instance to create a token with this button: [![Open your Home Assistant instance and show your Home Assistant user's profile.](https://my.home-assistant.io/badges/profile.svg)](https://my.home-assistant.io/redirect/profile/)
+
+`icon_color`: [optional] The color of the icon. Defaults to black. Add an RGB hex code, without the `#` sign
+
+`icon_size`: [optional] The width of the image in pixels, it's the `imageWidth` Argos parameter: [Argos help](https://github.com/p-e-w/argos#line-attributes)
+
+### Lines
+
+In this section just put the lines on the same order as they will show up on the dropdown. Possible lines:
+- `separator`
+- `entity`
+- `entities`
+
+#### Separator
+
+It's the same as an [argos separator](https://github.com/p-e-w/argos#rendering), and it behaves the same way: `entity` lines above the first separator display on the bar. If multiple `entity` lines are above the first `separator` they will cycle, and each will be displayed for 3 seconds.
+
+After the first `separator` latter ones will display as a horizontal line.
+
+#### Entity
+
+```yaml
+lines:
+  - entity: sensor.livingroom_temperature
+    attribute:
+      - state
+      - unit_of_measurement
+    icon: mdi:home-assistant
+  # Another example:
+  - entity: light.livingroom
+    service: light.toggle
+    icon: mdi:ceiling-light
+    name: Livingroom ceiling light
+    data:
+        brightness: 255
+```
+
+`entity`: [required] A entity id from home assistant
+
+`name`: [optional] The name how it should appear. If omitted, it will display the attribute selected. If attribute omitted, it will display the `friendly_name` attribute of the entity.
+
+`attribute`: [optional] An attribute or a list of attributes to get from homeassistant.
+
+`service`: [optional] A service to run when this line is clicked.
+
+`data`: [optional] Data for the service
+
+`icon:` [optional] An icon for that line. It depends on the prefix of this line, where it will get the icon:
+
+- `gtk:` A gtk icon will be displayed. See possible values on [freedesktop.org icon naming specification](https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html)
+- `mdi:` Material Design Icon, as on Home Assistant ui
+- Without prefix a base64 encoded image can be added
+
+#### Entities
+
+A dropdown list of other entities.
+
+```yaml
+lines:
+  - name: A list of entities
+    icon: gtk:view-list
+    entities:
+    - entity: sensor.mysensor
+      attribute: 
+    - entity: light.livingroom
+      service: light.toggle
+```
+
+`entities`: [required] A list of entities. Separator is also possible. All options available for them as normal `entity` lines.
+
+`name`: [optional] The title of the dropdown. 
+
+`icon`: [optional] Same as for `entity` lines
+
+## Troubleshooting
+
+Open an issue if something isn't working!
+
+### Running the script in terminal
+
+This script is an executable, so if you just simply run it in the terminal you should see some sane output.
+
+```shell
+cd ha-argos
+./ha-argos.py
+```
+
+### Cache
+
+The script caches the icons and `friendly_name`s of the entities to a `cache.json` file. If some icons or name is not updating, just simply delete this file, it will be recreated during next call to Home Assistant.
+
+
+## License
+
+MIT
---
+++ ha-argos.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import sys
+import yaml
+import json
+from collections import defaultdict
+import base64
+from lxml import etree as ET
+
+reload_string = '<span weight="bold">Reload</span> | refresh=true iconName=view-refresh-symbolic'
+
+# --------------------------------- Functions -------------------------------- #
+
+
+def call_ha(endpoint):
+    """Call homeassistant
+
+    Args:
+        endpoint (String): the endpoint to call
+
+    Returns:
+        Dict: The answer from the server as a dictionary
+    """
+    the_url = '/'.join([SERVER_URL, 'api', endpoint])
+    the_token = ' '.join(['Bearer', SERVER_TOKEN])
+    headers = {
+        "Authorization": the_token,
+        "content-type": "application/json",
+    }
+    response = requests.get(the_url, headers=headers)
+    return response.json()
+
+
+def get_ha_attr(attr,  entity_dict, use_cache=False):
+    """Get attribute values from config, cache or ha
+
+    Args:
+        attr (str or list): The attribute or the list of attributes
+        entity_dict (dict): The entity dictionary read from config file
+        use_cache (bool, optional): Use the cache for storing this attribute. Defaults to False.
+    Returns:
+        str: The attribute value
+    """
+    global CACHE_CHANGED
+
+    if not isinstance(attr, list):
+        # Return attribute value from config file:
+        if entity_dict[attr]:
+            return entity_dict[attr]
+
+        # Return attribute value from the cache:
+        if use_cache and CACHE[entity_dict['entity']]:
+            if CACHE[entity_dict['entity']][attr]:
+                return CACHE[entity_dict['entity']][attr]
+    # Call ha for attribute:
+    e_state = call_ha(f'states/{entity_dict["entity"]}')
+    if isinstance(attr, list):
+        e_vals = []
+        for att in attr:
+            if att == 'state':
+                e_vals.append(str(e_state[att]))
+            else:
+                e_vals.append(str(e_state['attributes'][att]))
+        # write to cache:
+        if use_cache:
+            CACHE_CHANGED = True
+            for att in attr:
+                if not att == 'state':
+                    CACHE[entity_dict['entity']][att] \
+                        = e_state['attributes'][att]
+        return e_vals
+    else:
+        if use_cache and not attr == 'state':
+            CACHE_CHANGED = True
+            CACHE[entity_dict['entity']] = dict()
+            CACHE[entity_dict['entity']][attr] = e_state['attributes'][attr]
+        return e_state['attributes'][attr]
+
+
+def print_line(line, submenu=False):
+    """Print a line from the yaml config
+
+    Args:
+        line (dict): Data from configuration.yaml
+        submenu (bool, optional): Print it to a submenu, for recursion. Defaults to False.
+    """
+    # It's a separator
+    if isinstance(line, str) and line == 'separator':
+        print('---')
+        return
+
+    cmd = defaultdict(bool, line)
+    output = []
+
+    # It's a list of other elements
+    if cmd['entities']:
+        # Add name or empty string to output
+        if cmd['name']:
+            output.append(cmd['name'])
+        else:
+            output.append(' ')
+
+        # Add icon:
+        if cmd['icon']:
+            output.extend(['|', print_icon(cmd['icon'])])
+        print((' ').join(output))
+
+        # Recurse into lines:
+        for e in cmd['entities']:
+            print_line(e, True)
+        return
+
+    # Add prefix if called from a submenu:
+    if submenu:
+        output.append('--')
+
+    # Add general prefix:
+    if cmd['prefix']:
+        output.append(cmd['prefix'])
+
+    # There is a command or an atribute:
+    if cmd['entity']:
+        # Display attribute value from ha:
+        if cmd['attribute']:
+            c_val = get_ha_attr(cmd['attribute'], cmd)
+            if isinstance(c_val, list):
+                c_val = ' '.join(c_val)
+            output.append(c_val)
+
+        # If no attribute but a name given, print that:
+        elif cmd['name']:
+            output.append(cmd['name'])
+
+        # No attribute, so display it's friendly_name from ha cache or from config:
+        else:
+            output.append(get_ha_attr('friendly_name', cmd, True))
+
+    # Display the name if not an entity
+    elif cmd['name']:
+        output.extend([cmd['name']])
+
+    # There is a call to a service:
+    if cmd['service'] and cmd['entity']:
+        output.append(
+            f'| bash=\"curl -X POST -H \\"Authorization: Bearer {SERVER_TOKEN}\\" -H \\"Content-Type: application/json\\"'
+        )
+        if cmd['data']:
+            output.append(
+                f'-d \'{{\\"entity_id\\": \\"{cmd["entity"]}\\",')
+            data_keys = [d for d in cmd['data']]
+            for i, d in enumerate(data_keys):
+                output.append(f'\\"{d}\\": \\"{cmd["data"][d]}\\"')
+                if i != len(data_keys) - 1:
+                    output.append(',')
+            output.append('}\'')
+        else:
+            output.append(
+                f'-d \'{{\\"entity_id\\": \\"{cmd["entity"]}\\"}}\''
+            )
+        output.append(
+            f'{SERVER_URL}/api/services/{cmd["service"].replace(".","/")}" terminal=false'
+        )
+    # No call, so add a separator:
+    else:
+        output.append('|')
+
+    # Add icon:
+    if cmd['icon']:
+        output.append(print_icon(cmd['icon']))
+
+    print((' ').join(output))
+
+
+def print_icon(icon_string):
+    """Print an icon from cache, gtk or mdi
+
+    Args:
+        icon_string (String): The name of the icon
+
+    Returns:
+        String: The icon as string, base64 or gtk...
+    """
+    if icon_string.startswith('gtk:'):
+        return f'iconName={icon_string[4:]}'
+    elif icon_string.startswith('mdi:'):
+        mdi_name = icon_string[4:]
+        # Try to find it in cache:
+        if CACHE[icon_string]:
+            return append_icon_size(f'image={CACHE[icon_string]}')
+        else:
+            icon_resp = requests.get(
+                f'https://raw.githubusercontent.com/Templarian/MaterialDesign-SVG/master/svg/{mdi_name}.svg')
+
+            # Build svg tree:
+            svg_root = ET.fromstring(icon_resp.content)
+
+            # Change svg color:
+
+            for t in svg_root:
+                if SETTINGS["icon_color"] and t.tag == '{http://www.w3.org/2000/svg}path':
+                    t.set('style', f'fill:#{SETTINGS["icon_color"]}')
+
+            # svg to string:
+            icon_str = ET.tostring(
+                svg_root,
+                encoding='utf-8',
+                method='xml',
+                xml_declaration=True
+            )
+
+            # Encode and cache svg
+            icon_b = base64.b64encode(icon_str).decode("utf-8")
+            global CACHE_CHANGED
+            CACHE_CHANGED = True
+            CACHE[icon_string] = icon_b
+            return append_icon_size(f'image={icon_b}')
+    else:
+        return append_icon_size(f'image={icon_string}')
+
+
+def append_icon_size(image_string):
+    """Append icon size based on config
+
+    Args:
+        image_string (String): The icon as string
+
+    Returns:
+        String: The icon as string with size
+    """
+    if SETTINGS["icon_size"]:
+        return f'{image_string}, imageWidth={SETTINGS["icon_size"]}'
+    else:
+        return image_string
+
+
+# -------------------------- Open configuration.yaml ------------------------- #
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    with open(os.path.join(script_dir, 'configuration.yaml'), 'r') as config_file:
+        config = defaultdict(bool, yaml.safe_load(config_file))
+
+except FileNotFoundError:
+    print('No config!')
+    print('---')
+    print('Configuration.yaml not found. Please create one!')
+    print('It should be in this directory:')
+    print(script_dir)
+    print('---')
+    print(reload_string)
+    exit()
+
+# ------------------------------ Open cache file ----------------------------- #
+
+try:
+    with open(os.path.join(script_dir, 'cache.json'), 'r') as cache_file:
+        json_dict = json.load(cache_file)
+    CACHE = defaultdict(bool, json_dict)
+
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    CACHE = defaultdict(lambda: defaultdict(bool))
+
+
+CACHE_CHANGED = False
+
+# --------------------------- Check server settings -------------------------- #
+
+if not config['settings']['url'] or not config['settings']['token']:
+    print('No credentials!')
+    print('---')
+    print('Server url or token missing from cofiguration.yaml')
+    print('---')
+    print(reload_string)
+    exit()
+
+# Sanitize:
+SERVER_URL = config['settings']['url']
+if SERVER_URL[-1] == '/':
+    SERVER_URL = SERVER_URL[:-1]
+SERVER_TOKEN = config['settings']['token']
+SETTINGS = defaultdict(bool, config['settings'])
+
+
+# ----------------------------------- Lines ---------------------------------- #
+
+if config['lines']:
+    for l in config['lines']:
+        print_line(l)
+else:
+    print('HA')
+
+
+print('---')
+print(reload_string)
+
+# -------------------------------- Write cache ------------------------------- #
+
+if CACHE_CHANGED:
+    cache_json = json.dumps(CACHE)
+
+    with open(os.path.join(script_dir, 'cache.json'), "w") as outfile:
+        outfile.write(cache_json)
